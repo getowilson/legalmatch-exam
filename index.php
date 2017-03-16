ca1ef9b0adba485756c8b769fd4a6841d64bcea2
redpaint_wilson <wilson@cloudemployee.co.uk>
2017-03-16 12:49:12 +0800

    BREAK

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Legal Match</title>
+	<meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content=""/>
+
+	<!-- Load styles-->
+	<link rel='stylesheet'  href='css/font-awesome.min.css' type='text/css' media='all' />
+	<link rel='stylesheet'  href='css/bootstrap.min.css' type='text/css' media='all' />
+	<link rel='stylesheet'  href='css/global.css' type='text/css' media='all' />
+	<link rel='stylesheet'  href='css/mobile.css' type='text/css' media='all' />
+
+</head>
+<body>
+
+	<div class="table-display-wrapper">
+		<div class="table-top">
+
+			<!-- header -->
+			<header class="header">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-9">
+							<div class="logo">
+								<a href="#"><img class="img-responsive" src="./images/logo-phil.jpg" alt="Legal Match Philippines" title="Legal Match Philippines" /></a>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<ul class="guaranteed-list">
+								<li>No fee to present your case</li>
+								<li>Choose from lawyers in your area</li>
+								<li>A 100% confidential services</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</header><!--/ header -->
+
+			<!-- .main-entry -->
+			<section class="main-entry">
+				<div class="container">
+					<div class="page-header">
+						<h1>Find a Lawyer for your Legal Issue</h1>
+						<h2>Fast, Free and Confidential</h2>
+					</div>
+					<div class="row">
+						<div class="col-md-6 intake-form">
+							<form action="" method="GET">
+								<div class="form-field">
+									<label for="zipcode">Enter Zip Code or City:</label>
+									<div class="input-group">
+									  <span class="input-group-addon" id="zipcode">1</span>
+									  <input type="text" class="form-control" placeholder="Does not have to be where you live" aria-describedby="zipcode">
+									</div>
+								</div>
+								<div class="form-field">
+									<label for="category">Choose a category:</label>
+									<div class="dropdown input-group">
+										<span class="input-group-addon" id="category">2</span>
+									  <button class="btn dropdown-toggle" type="button" id="category" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+									    Click to choose a legal category
+									    <span class="fa fa-chevron-down"></span>
+									  </button>
+									  <ul class="dropdown-menu" aria-labelledby="category">
+									    <li><a href="#">Category 1</a></li>
+									    <li><a href="#">Category 2</a></li>
+									    <li><a href="#">Category 3</a></li>
+									    <li><a href="#">Category 4</a></li>
+									    <li><a href="#">Category 5</a></li>
+									  </ul>
+									</div>
+								</div>
+								<p class="legend text-center">Can't find your category? <a href="#">Click here.</a></p>
+							</form>
+						</div>
+						<div class="col-md-5 col-md-offset-1 client-reviews">
+							<h3>Client review LegalMatch lawyers</h3>
+							<div class="row">
+								<div class="col-xs-3 lawyer-info">
+									<img src="./images/Mitchell.png" alt="Mitchell M." />
+									<span class="name">Mitchell M.</span>
+									<span class="address">Cherry Hill, NJ</span>
+								</div>
+								<div class="col-xs-9 lawyer-desc">
+									<strong>Family Law</strong>
+									<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ...</p>
+									<a href="#" class="review-btn">Read Review</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section><!--/ .main-entry -->
+
+		</div><!-- /.table-top -->
+		<div class="table-bottom">
+			<!-- footer -->
+			<footer class="footer">
+				<div class="container">
+					fasddfsadf
+				</div>
+			</footer><!--/ footer -->
+		</div>
+	</div><!-- /.table-display-wrapper --> 
+
+	<!-- Load scripts -->
+	<script type='text/javascript' src='js/jquery.js'></script>
+	<script type='text/javascript' src='js/bootstrap.min.js'></script>
+	<script type='text/javascript' src='js/global.js'></script>
+
+</body>
+</html>
---
+++ js/global.js
@@ -0,0 +1,10 @@
+/* Initialize and/or execute, code jQuery Scripts here */
+jQuery( function($) {
+
+	$(document).ready(function() {	
+
+		
+			
+	});
+	
+}); // End of jQuery ready domain
